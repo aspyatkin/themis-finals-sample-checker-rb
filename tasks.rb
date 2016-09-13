f597f2d16cf0041684a5c0e876b046300d967e0f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-13 18:27:08 +0000

    Sentry stuff

--- tasks.rb
+++ tasks.rb
@@ -4,6 +4,7 @@ require 'time_difference'
 require 'net/http'
 require 'json'
 require './utils'
+require 'raven/base'
 
 config_module_name = ENV['THEMIS_FINALS_CHECKER_MODULE'] || ::File.join(
   Dir.pwd,
@@ -11,7 +12,7 @@ config_module_name = ENV['THEMIS_FINALS_CHECKER_MODULE'] || ::File.join(
 )
 require config_module_name
 
-logger = get_logger
+$logger = get_logger
 
 ::Sidekiq.default_worker_options = { 'retry' => 0 }
 
@@ -21,13 +22,13 @@ logger = get_logger
   }
 
   config.on(:startup) do
-    logger.info "Starting queue process, instance #{ENV['QUEUE_INSTANCE']}"
+    $logger.info "Starting queue process, instance #{ENV['QUEUE_INSTANCE']}"
   end
   config.on(:quiet) do
-    logger.info 'Got USR1, stopping further job processing...'
+    $logger.info 'Got USR1, stopping further job processing...'
   end
   config.on(:shutdown) do
-    logger.info 'Got TERM, shutting down process...'
+    $logger.info 'Got TERM, shutting down process...'
   end
 end
 
@@ -35,6 +36,19 @@ end
   config.redis = {
     url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}"
   }
+end
+
+$raven_enabled = !ENV['SENTRY_DSN'].nil?
+
+if $raven_enabled
+  ::Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.ssl_verification = false
+    config.logger = $logger
+    config.async = lambda { |event|
+      ::Thread.new { ::Raven.send_event(event) }
+    }
+  end
 end
 
 class Push
@@ -48,8 +62,11 @@ class Push
     rescue Interrupt
       raise
     rescue Exception => e
-      logger.error e.message
-      e.backtrace.each { |line| logger.error line }
+      if $raven_enabled
+        ::Raven.capture_exception e
+      end
+      $logger.error e.message
+      e.backtrace.each { |line| $logger.error line }
     end
 
     return result, updated_adjunct
@@ -99,7 +116,37 @@ class Push
         processing_time
       ]
 
-    logger.error log_message
+    if $raven_enabled
+      short_log_message = \
+        'PUSH `%s...` /%d to `%s` - status %s' % [
+          params['flag'][0..7],
+          metadata['round'],
+          metadata['team_name'],
+          ::Themis::Finals::Checker::Result.key(status)
+        ]
+
+      raven_data = {
+        level: 'info',
+        tags: {
+          tf_operation: 'push',
+          tf_status: ::Themis::Finals::Checker::Result.key(status).to_s,
+          tf_team: metadata['team_name'],
+          tf_service: metadata['service_name'],
+          tf_round: metadata['round']
+        },
+        extra: {
+          endpoint: params['endpoint'],
+          flag: params['flag'],
+          adjunct: job_result[:adjunct],
+          delivery_time: delivery_time,
+          processing_time: processing_time
+        }
+      }
+
+      ::Raven.capture_message short_log_message, raven_data
+    end
+
+    $logger.error log_message
 
     uri = URI(job_data['report_url'])
 
@@ -112,7 +159,7 @@ class Push
       http.request(req)
     end
 
-    logger.error res.value
+    $logger.error res.value
   end
 end
 
@@ -126,8 +173,11 @@ class Pull
     rescue Interrupt
       raise
     rescue Exception => e
-      logger.error e.message
-      e.backtrace.each { |line| logger.error line }
+      if $raven_enabled
+        ::Raven.capture_exception e
+      end
+      $logger.error e.message
+      e.backtrace.each { |line| $logger.error line }
     end
 
     result
@@ -176,7 +226,37 @@ class Pull
         processing_time
       ]
 
-    logger.info log_message
+    if $raven_enabled
+      short_log_message = \
+        'PULL `%s...` /%d from `%s` - status %s' % [
+          params['flag'][0..7],
+          metadata['round'],
+          metadata['team_name'],
+          ::Themis::Finals::Checker::Result.key(status)
+        ]
+
+      raven_data = {
+        level: 'info',
+        tags: {
+          tf_operation: 'pull',
+          tf_status: ::Themis::Finals::Checker::Result.key(status).to_s,
+          tf_team: metadata['team_name'],
+          tf_service: metadata['service_name'],
+          tf_round: metadata['round']
+        },
+        extra: {
+          endpoint: params['endpoint'],
+          flag: params['flag'],
+          adjunct: params['adjunct'],
+          delivery_time: delivery_time,
+          processing_time: processing_time
+        }
+      }
+
+      ::Raven.capture_message short_log_message, raven_data
+    end
+
+    $logger.info log_message
 
     uri = URI(job_data['report_url'])
 
@@ -189,6 +269,6 @@ class Pull
       http.request(req)
     end
 
-    logger.error res.value
+    $logger.error res.value
   end
 end
