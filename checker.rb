6f0c09379e0f9e19c6ef99561ff7ddd88e8c6d9f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-05-31 11:38:21 +0000

    Add debug logging

--- checker.rb
+++ checker.rb
@@ -1,4 +1,7 @@
 require 'themis/finals/checker/result'
+require './utils'
+
+$logger = get_logger
 
 def get_random_message
   range = [*'0'..'9',*'A'..'Z',*'a'..'z']
@@ -6,11 +9,13 @@ def get_random_message
 end
 
 def push(endpoint, capsule, label, metadata)
+  $logger.debug('PUSH capsule: ' + capsule)
   sleep ::Random.new.rand 1..5
   return ::Themis::Finals::Checker::Result::UP, label, get_random_message
 end
 
 def pull(endpoint, capsule, label, metadata)
+  $logger.debug('PULL capsule: ' + capsule)
   sleep ::Random.new.rand 1..5
   return ::Themis::Finals::Checker::Result::UP, get_random_message
 end
